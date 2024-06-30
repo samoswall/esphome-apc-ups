afda6afc4c34413e4c643b6b66b875b4f2d8fe1d
samoswall <80251553+samoswall@users.noreply.github.com>
2024-07-01 00:43:19 +0300

    Update __init__.py

--- components/apc_ups/switch/__init__.py
+++ components/apc_ups/switch/__init__.py
@@ -13,7 +13,7 @@ CONF_DEEP_TEST = "deep_test"
 CONF_TEN_MINUTES_TEST = "ten_minutes_test"
 
 TYPES = {
-    CONF_BEEPER: ("1", "0"),
+    CONF_BEEPER: ("0", "1"),
     CONF_QUICK_TEST: ("T", "CT"),
     CONF_DEEP_TEST: ("TL", "CT"),
     CONF_TEN_MINUTES_TEST: ("T10", "CT"),
