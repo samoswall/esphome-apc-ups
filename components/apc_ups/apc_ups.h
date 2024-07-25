33ecc8db637acf4edcb97d3c4988fc7cf3e9e9e3
samoswall <80251553+samoswall@users.noreply.github.com>
2024-07-25 21:43:20 +0300

    Update apc_ups.h

--- components/apc_ups/apc_ups.h
+++ components/apc_ups/apc_ups.h
@@ -4,7 +4,6 @@
 #include "esphome/components/sensor/sensor.h"
 #include "esphome/components/switch/switch.h"
 #include "esphome/components/text_sensor/text_sensor.h"
-#include "esphome/components/select/select.h"
 #include "esphome/components/uart/uart.h"
 #include "esphome/core/automation.h"
 #include "esphome/core/component.h"
